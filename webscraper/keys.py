1f750e9b892226c2f144d460f06359b220ed1b7e
Rey Christopher D. Aparece <33944673+Apacher122@users.noreply.github.com>
2022-02-28 04:44:37 -0500

    Create keys.py

---
+++ webscraper/keys.py
@@ -0,0 +1,8 @@
+# SECURE YOUTUBE API KEY HERE
+youTubeAPIKey = 
+
+# Channel ID of you're choosing
+youtubeID = "UCwG60McbuKGsj53_JLoY-9Q"
+
+# Use to parse all videos
+listID = "UUwG60McbuKGsj53_JLoY-9Q"
